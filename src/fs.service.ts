9fd500ad83131e92c44e8ff446af211b6478fec6
YuliiaShpytsia <yulia93shpiza@gmail.com>
2024-09-25 23:31:09 +0300

    lesson-3

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,22 +1,26 @@
-const fs = require('node:fs/promises');
-const path  = require('node:path')
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-module.exports = {
-    read: async () => {
+import {IUser} from './interfaces/user.interface'
+
+const read =  async (): Promise<IUser[]> => {
         try{
-            const pathToFile = path.join(__dirname, 'db.json');
+            const pathToFile = path.join(process.cwd(), 'db.json');
             const data = await fs.readFile(pathToFile, 'utf-8');
             return data ? JSON.parse(data) : [];
         } catch (e) {
             console.log('Ошибка записи', e.message);
         }
-    },
-    write: async (users) => {
+    };
+
+const write = async (users:IUser[]): Promise<void> => {
         try {
-            const pathToFile = path.join(__dirname, 'db.json');
+            const pathToFile = path.join(process.cwd(), 'db.json');
             await fs.writeFile(pathToFile, JSON.stringify(users))
         } catch (e) {
             console.log('Ошибка записи', e.message);
         }
-    }
-}
+    };
+
+export {read, write}
+
